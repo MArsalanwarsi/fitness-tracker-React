a5eb63f0c60218b73fed348525f309cf41b1e81e
MArsalanwarsi <mohammadarsalanwarsi@gmail.com>
2026-03-29 01:50:52 +0500

    added analytics

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,7 @@ import Dashboard from "./src/routers/Dashboard.js";
 const app = express();
 const port = process.env.PORT || 1000;
 app.use(cors({
-    origin: ["http://localhost:5173", "https://fitness-tracker-react.vercel.app"],
+    origin: ["http://localhost:5173", "https://fitness-tracker-react-seven.vercel.app"],
     credentials: true,
 }));
 app.use(express.json());
