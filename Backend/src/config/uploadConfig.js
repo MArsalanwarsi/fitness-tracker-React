05266d283ee65a7e577e6f1f7869cc69e4dba9de
MArsalanwarsi <mohammadarsalanwarsi@gmail.com>
2026-03-29 01:06:18 +0500

    process updated

--- Backend/src/config/uploadConfig.js
+++ Backend/src/config/uploadConfig.js
@@ -4,14 +4,14 @@ import multer from "multer";
 
 
 cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
-    folder: 'fitness-tracker-uploads', 
+    folder: 'fitness-tracker-uploads',
     allowed_formats: ['jpg', 'png', 'jpeg', 'JPG', 'PNG', 'JPEG'],
   },
 });
--- Backend/src/routers/Auth.js
+++ Backend/src/routers/Auth.js
@@ -1,6 +1,6 @@
 import express from "express";
-import { Register, Login, Profile,UpdateProfileImage } from "../controllers/AuthContoller.js";
-import  authMiddleware  from "../middleware/authMiddleware.js";
+import { Register, Login, Profile, UpdateProfileImage } from "../controllers/AuthContoller.js";
+import authMiddleware from "../middleware/authMiddleware.js";
 import roleMiddleware from "../middleware/roleMiddleware.js";
 import uploadConfig from "../config/uploadConfig.js";
 import { ForgotPassword, VerifyOTP, ResetPassword } from "../controllers/ForgotContoller.js";
@@ -13,5 +13,5 @@ authRouter.post("/forgotPassword", ForgotPassword);
 authRouter.post("/verifyOTP", VerifyOTP);
 authRouter.put("/resetPassword", ResetPassword);
 authRouter.get("/profile", authMiddleware, roleMiddleware(["user", "admin"]), Profile);
-authRouter.put("/profile/image",authMiddleware,uploadConfig.single('profile_pic'),UpdateProfileImage);
+authRouter.put("/profile/image", authMiddleware, uploadConfig.single('profile_pic'), UpdateProfileImage);
 export default authRouter
